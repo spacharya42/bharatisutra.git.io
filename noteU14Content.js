6ff47a453563fdc8132d3b6b20aae0a6ce7d5b25
spacharya42 <spacharya42@gmail.com>
2026-01-24 20:34:53 +0545

    Create noteU14Content.js

---
+++ noteU14Content.js
@@ -0,0 +1,2 @@
+window.noteU13Content = `# Security Patterns Added (UNLocked)\n\n## Auth Table\n\n| Method | Level |\n|--------|-------|\n| JWT | High |\n| OAuth | Very High |`;
+window.noteU13Meta = { name: "Security ok", emoji: "🔒", password: null };
